43832e416b7c2f1de26a5c02f25e73b269a43772
Nick Balestra <nick@balestra.ch>
2015-07-24 22:02:32 -0700

    Refactor anc cleanup, synced with the backboenFire branch

--- Client/collections/colorsCollection.js
+++ Client/collections/colorsCollection.js
@@ -1,3 +1,11 @@
+// Firebase collection binding via backbonefire
+//=============================================
+//
+// var Colors = Backbone.Firebase.Collection.extend({
+//   url: 'https://firebone.firebaseio.com/colors',
+//   model: Color,
+// });
+
 var Colors = Backbone.Collection.extend({
   model: Color,
 });
--- Client/views/appView.js
+++ Client/views/appView.js
@@ -2,15 +2,17 @@ var AppView = Backbone.View.extend({
   template: _.template('<div class="row" style="background-color:<%= hexColor %>"><span><%= hexColor %></span></div>'),
 
   events: {
-    'click .addColor': 'addColor'
+    'click #add': 'addColor',
+    'click #remove': 'remove',
+    'click #reset': 'reset'
   },
 
   initialize: function(){
-    this.collection.on('add', this.render, this);
+    this.collection.on('add remove reset', this.render, this);
   },
 
   render: function(){
-    $view = this.$el.html('<div class="addColor"><span>Gimme moar<span></div><div id="colors"></div>');
+    $view = this.$el.html('<div class="addColor"><span id="add">Gimme moar</span><span id="remove">Gimme less</span><span id="reset">Gimme me none</span></div><div id="colors"></div>');
 
     this.collection.each(function(color){
       $view.find('#colors').prepend(this.template(color.attributes));
@@ -19,8 +21,16 @@ var AppView = Backbone.View.extend({
     return $view;
   },
 
-
   addColor: function(){
-    this.collection.add(new Color());
+    var randomHex = Please.make_color({base_color: 'cyan'});
+    this.collection.add({hexColor: randomHex});
+  },
+
+  remove: function(){
+    this.collection.pop();
+  },
+
+  reset: function(){
+    this.collection.reset();
   }
 });
